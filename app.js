f8a5449fa53bf8d564033fd661a6f56ad612519e
Randriakalomalala Ainasafidy <37513343+lameoxyde@users.noreply.github.com>
2021-08-19 17:22:40 +0300

    add global middleware
    - add global middleware
    - add log on console log requestion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Node.js ==> Express Framework (SIMPLE SERVER)
 const express = require("express"),
-      Twig = require("twig")
+    Twig = require("twig")
 let app = express();
 //Port to listen on
 const PORT = 8000;
@@ -25,6 +25,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //Create Express Router
 const router = express.Router();
+
+//global Middleware
+var date = new Date(Date.now() * 1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+
+router.use(function(req, res, next) {
+    console.log('Time:', formattedTime + ' : ' + req.protocol + '://' + req.get('host') + req.originalUrl);
+    next();
+});
 app.use(router);
 
 //fichier assert
@@ -37,17 +55,17 @@ bootstrap(app, router);
 
 //Main Page (Home)
 router.get("/", (req, res, next) => {
-  return res.send("Hello There");
+    return res.send("Hello There");
 });
 
 router.use((err, req, res, next) => {
-  if (err) {
-    //Handle file type and max size of image
-    return res.send(err.message);
-  }
+    if (err) {
+        //Handle file type and max size of image
+        return res.send(err.message);
+    }
 });
 
 app.listen(PORT, err => {
-  if (err) return console.log(`Cannot Listen on PORT: ${PORT}`);
-  console.log(`Server is Listening on: http://localhost:${PORT}/`);
+    if (err) return console.log(`Cannot Listen on PORT: ${PORT}`);
+    console.log(`Server is Listening on: http://localhost:${PORT}/`);
 });
